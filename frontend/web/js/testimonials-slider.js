8d0425be94d66e1ed6d1bd75f3d1fb7a9b80337e
Артем Омельченко <C:\Users\artem\AppData\Roaming\The Bat!>
2020-03-12 10:58:44 +0200

    'fix_settings_frontend'

---
+++ frontend/web/js/testimonials-slider.js
@@ -0,0 +1,21 @@
+//Testimonials
+$(document).ready(function(){
+    $(".testimonials").owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      dots: true,
+      center:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
+    });
+  });
